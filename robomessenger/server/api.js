86b3cf90f7aa7c386bf938bee465cdc74bf77b53
forget-ful-day <z.max.evg@gmail.com>
2026-02-14 17:53:22 +0300

    15

--- robomessenger/server/api.js
+++
@@ -1,8 +0,0 @@
-// JavaScript файл: api.js
-console.log("Файл api.js загружен");
-
-function init() {
-    console.log("Приложение инициализировано");
-}
-
-document.addEventListener('DOMContentLoaded', init);
--- robomessenger/server/message.js
+++
@@ -1,8 +0,0 @@
-// JavaScript файл: message.js
-console.log("Файл message.js загружен");
-
-function init() {
-    console.log("Приложение инициализировано");
-}
-
-document.addEventListener('DOMContentLoaded', init);
